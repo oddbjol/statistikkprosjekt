0dd84e412ee006b7502f84e25b327a0c1e117f2a
odd <oddbjool@stud.ntnu.no>
2018-02-05 12:37:15 +0100

    kim sine data ++

--- Data.py
+++ Data.py
@@ -12,6 +12,15 @@ kontakt = pd.read_csv('kontakt.csv', index_col=0, comment='#')
 # prosentandel nordmenn som har positiv holdning til innvandrere
 holdning = pd.read_csv('holdning.csv', index_col=0, comment='#')
 
+# Antall minutter brukt på joggetur, 5 intervaller totalt.
+joggetur = pd.read_csv('joggetur.csv', index_col=0, comment='#')
+
+# Nedbør (mm) i januar fra 1890 til 1994
+nedbor = pd.read_csv('nedbor.csv', index_col=0, comment='#')
+
+# Antall observerte elbiler (av totalt 100 biler)
+biler = pd.read_csv('biler.csv', index_col=0, comment='#')
+
 
 # Tegner en graf/figur til en pdf-fil
 def draw_graph(data, pdf, param_dict=None):
@@ -28,9 +37,18 @@ def draw_graph(data, pdf, param_dict=None):
     gjennomsnitt = [round(data.mean()[0], 2)]
     median = [data.median()[0]]
     modus = data.mode().values
-    variasjonsbredde = data.max() - data.min()
-    varians = data.var()
+    modus = [[item for sublist in modus for item in sublist]]
+    variasjonsbredde = round(data.max() - data.min(),2)
+    varians = round(data.var(),2)
     standardavvik = round(data.std(), 2)
+    print('\n\n\n\n')
+
+    print('gjennomsnitt : ', gjennomsnitt)
+    print('median : ', median)
+    print('modus : ', modus)
+    print('variasjonsbredde : ', variasjonsbredde)
+    print('varians : ', varians)
+    print('standardavvik : ', standardavvik)
 
     ax[1].table(
         rowLabels=['gjennomsnitt', 'median', 'modus', 'variasjonsbredde', 'varians', 'standardavvik'],
@@ -49,6 +67,9 @@ def main():
         draw_graph(lapper, pdf, {'kind': 'bar'})
         draw_graph(kontakt, pdf)
         draw_graph(holdning, pdf)
+        draw_graph(joggetur, pdf)
+        draw_graph(nedbor, pdf)
+        draw_graph(biler, pdf, {'kind': 'bar'}) # TODO: Lag kakediagram av denne.
 
         print("PDF SAVED!")
 
