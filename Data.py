59380214a5d191e70c50ce93edcc3213fec255d5
odd <oddbjool@stud.ntnu.no>
2018-02-05 14:00:33 +0100

    mer greier

--- Data.py
+++ Data.py
@@ -18,29 +18,36 @@ joggetur = pd.read_csv('joggetur.csv', index_col=0, comment='#')
 # Nedbør (mm) i januar fra 1890 til 1994
 nedbor = pd.read_csv('nedbor.csv', index_col=0, comment='#')
 
+# Antall sykler observert på 30 min
+sykler = pd.read_csv('sykler.csv', index_col=0, comment='#')
+
 # Antall observerte elbiler (av totalt 100 biler)
 biler = pd.read_csv('biler.csv', index_col=0, comment='#')
 
 
 # Tegner en graf/figur til en pdf-fil
-def draw_graph(data, pdf, param_dict=None):
+def draw_graph(data, pdf, param_dict=None, table_data=None, title=''):
     if param_dict is None:
         param_dict = {}
+
+    if table_data is None:
+        table_data = data
 
     fig, ax = plt.subplots(2)
     data.plot(ax=ax[0], **param_dict)
 
+    fig.suptitle(title)
     plt.rcParams.update({'legend.fontsize': 6})
 
     ax[1].axis('off')
 
-    gjennomsnitt = [round(data.mean()[0], 2)]
-    median = [data.median()[0]]
-    modus = data.mode().values
+    gjennomsnitt = [round(table_data.mean()[0], 2)]
+    median = [table_data.median()[0]]
+    modus = table_data.mode().values
     modus = [[item for sublist in modus for item in sublist]]
-    variasjonsbredde = round(data.max() - data.min(),2)
-    varians = round(data.var(),2)
-    standardavvik = round(data.std(), 2)
+    variasjonsbredde = round(table_data.max() - table_data.min(),2)
+    varians = round(table_data.var(),2)
+    standardavvik = round(table_data.std(), 2)
     print('\n\n\n\n')
 
     print('gjennomsnitt : ', gjennomsnitt)
@@ -69,7 +76,8 @@ def main():
         draw_graph(holdning, pdf)
         draw_graph(joggetur, pdf)
         draw_graph(nedbor, pdf)
-        draw_graph(biler, pdf, {'kind': 'bar'}) # TODO: Lag kakediagram av denne.
+        draw_graph(biler, pdf, {'kind': 'pie', 'y': 'antall'}, biler.drop('vanlig bil'))  # TODO: Lag kakediagram av denne.
+        draw_graph(sykler, pdf, {'kind': 'bar'})
 
         print("PDF SAVED!")
 
